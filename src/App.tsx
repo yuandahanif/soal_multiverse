3d01a4607556f9506b1a16eba3c6f0cda472c061
yuanda <34027873+yuandahanif@users.noreply.github.com>
2022-05-08 04:10:14 +0700

    testing sesuatu

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,98 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "react-query";
 import "./App.css";
 import fetchJson from "./libs/fetcher";
 
-const AnswerPopup = () => {
+const AnswerPopup = ({ children }: { children?: React.ReactNode }) => {
   return (
     <div className="popup-answer-container">
-      <div className="popup-answer">
-        <button className="button-choice">ayam</button>
-        <button className="button-choice">telur</button>
-      </div>
+      <div className="popup-answer">{children}</div>
     </div>
   );
 };
 
 function App() {
-  const [urls, setUrls] = useState([
-    "https://raw.githubusercontent.com/yuandahanif/soal_multiverse/story/public/story.json",
-  ]);
+  const [url, setUrl] = useState<string | null>(
+    "https://raw.githubusercontent.com/yuandahanif/soal_multiverse/story/public/story.json"
+  );
 
-  const query = useQuery("main_story", () => fetchJson(urls[urls.length - 1]));
+  const [stories, setStories] = useState<{ [key: string]: any }[]>([]);
+  const [answers, setAnswers] = useState<any[]>([]);
+
+  const [answerOptions, setAnsweroptions] = useState<any[] | null>(null);
+
+  const answerQuestion = (answer: any) => {
+    setAnswers(answer.text);
+    setUrl(answer.url);
+  };
+
+  const askQuestion = (options: any) => {
+    setAnsweroptions(options);
+  };
+
+  const closeQuestion = () => {
+    setAnsweroptions(null);
+  };
+
+  const query = useQuery("main_story", () => fetchJson(url!), {
+    enabled: url !== null,
+  });
+
+  useEffect(() => {
+    if (query.isSuccess && url !== null) {
+      setStories((s) => {
+        const newStory: any = {};
+        newStory[url] = query.data;
+        return { ...s };
+      });
+    }
+  }, [query.data, query.isSuccess, url]);
 
   return (
     <div className="App">
-      <AnswerPopup />
+      {answerOptions && (
+        <AnswerPopup>
+          <button
+            type="button"
+            className="button-choice"
+            onClick={() => answerQuestion(answerOptions[0])}
+          >
+            {answerOptions[0]?.text}
+          </button>
+          <button
+            type="button"
+            className="button-choice"
+            onClick={() => answerQuestion(answerOptions[1])}
+          >
+            {answerOptions[1]?.text}
+          </button>
+          <button className="button-choice" onClick={closeQuestion}>
+            tutup
+          </button>
+        </AnswerPopup>
+      )}
+
       <div className="container handwriting-font">
-        {query.isSuccess && query.data?.main_story}
-        <br />
-        <button className="button-answer redacted-font">[redacted]</button>
+        {stories &&
+          stories.map((story, index) => (
+            <div key={story.id}>
+              {query.isSuccess && query.data?.main_story.split("[pilihan]")[0]}
+              {answers[index] ? (
+                answers[index]?.text
+              ) : (
+                <button
+                  className="button-answer redacted-font"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    askQuestion(query.data.options);
+                  }}
+                >
+                  [redacted]
+                </button>
+              )}
+              {query.isSuccess && query.data?.main_story.split("[pilihan]")[1]}
+            </div>
+          ))}
       </div>
     </div>
   );
