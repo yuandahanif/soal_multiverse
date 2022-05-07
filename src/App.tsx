543e67ebbed2a92fd2c70fda5fdb0dc413ddb758
yuanda <34027873+yuandahanif@users.noreply.github.com>
2022-05-08 03:20:25 +0700

    add popup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import { useQuery, useMutation, useQueryClient } from "react-query";
 import "./App.css";
 import fetchJson from "./libs/fetcher";
 
+const AnswerPopup = () => {
+  return (
+    <div className="popup-answer-container">
+      <div className="popup-answer">
+        <button className="button-choice">ayam</button>
+        <button className="button-choice">telur</button>
+      </div>
+    </div>
+  );
+};
+
 function App() {
   const url =
     "https://raw.githubusercontent.com/yuandahanif/soal_multiverse/story/public/story.json";
@@ -9,8 +20,11 @@ function App() {
   const query = useQuery("todos", () => fetchJson(url));
   return (
     <div className="App">
-      <div className="container">
+      <AnswerPopup />
+      <div className="container handwriting-font">
         {query.isSuccess && query.data?.main_story}
+        <br />
+        <button className="button-answer redacted-font">[redacted]</button>
       </div>
     </div>
   );
